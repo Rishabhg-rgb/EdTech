39798ec5d63eea7a51f78cfcd9a3036e8f277a34
Rishabh <rishabhgoswami901@gmail.com>
2022-11-01 00:29:48 +0530

    my commit rishabh

--- src/Component/Mocktest.js
+++ src/Component/Mocktest.js
@@ -9,6 +9,7 @@ const Mocktest = () => {
         const data = await fetch("http://localhost:5000/api/ques/getquestions")
         const parsedData = await data.json()
         setQuestions(parsedData)
+
     }
     useEffect(() => {
       fetchQuestions()
